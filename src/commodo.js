72b3915a6be8ab3227bf8a2168ee6bf587361b09
Contra <contra@maricopa.edu>
2011-09-08 13:10:49 -0700

    clean

--- src/commodo.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  require.module('commodo', function(module, exports, require) {
-    return module.exports = {
-      version: '0.0.1',
-      author: 'Contra'
-    };
-  });
-}).call(this);
